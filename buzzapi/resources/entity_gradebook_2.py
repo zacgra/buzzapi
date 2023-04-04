69f1f1f29529c998a275256c8f4637ec77808f19
Zach Grammon <zachgrammon@gmail.com>
2023-04-04 14:09:29 -0600

    Add getentitygradebook2 to resources and update client with EntityGradebook2

---
+++ buzzapi/resources/entity_gradebook_2.py
@@ -0,0 +1,21 @@
+from urllib.parse import urlencode
+
+
+class EntityGradebook2:
+    def get_entity_gradebook_2(self, entityid: int, params: dict):
+        """Gets grades for all students enrolled in specified entity.
+        The entity id can be a Course ID, Section ID, or Group ID
+
+        Args:
+            entityid (int): Course ID, Section ID, or Group ID
+            params (dict): Additional args as dict
+                           ex: {'itemid': 12345 }
+        Returns:
+            dict: List of enrollments un
+        """
+        query = {
+            "cmd": "getentitygradebook2",
+            "entityid": entityid,
+            **params,
+        }
+        return self.get(urlencode(query))
